a494befb9a4af6842d46dc60189172902225eb8c
Brandon Silvibarr <brandon.silvi@gmail.com>
2026-02-25 17:10:40 -0800

    initial commit, part A arrays completed.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace COMP003A.LectureActivity7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // --------- A) Array
+
+            int[] scores = { 85, 90, 708, 92, 88 };
+            
+            Console.WriteLine("Array values using for loop");
+
+            for (int i = 0; i < scores.Length; i++)
+            {
+                Console.WriteLine(scores[i]);
+            }
+        }
+    }
+}
